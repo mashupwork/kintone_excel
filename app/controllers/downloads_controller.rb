937eae1830dda554b9994e52f6446ae1149e3a66
Hironobu Nishikokura <nishiko@mindia.jp>
2016-06-14 19:34:13 +0900

    枠線まで検証完了

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,5 +1,6 @@
 class DownloadsController < ApplicationController
   def show
+    id = params[:id]
     send_file Xlsx.file(id, "#{id}.xlsx")
   end
 end
--- app/models/xlsx.rb
+++ app/models/xlsx.rb
@@ -1,35 +1,39 @@
 class Xlsx
   def self.file_names
+    res = []
     path = Rails.root.join('tmp', 'xlsx')
     Dir.open(path) do |dir|
       dir.each do |f|
         next if f.match(/^~/)
         next unless f.match(/xlsx$/)
-        puts f
+        res.push(f)
       end
     end
+    res
   end
 
-  def file filename, template
+  def self.file filename, template
     workbook = RubyXL::Parser.parse(Rails.root.join('tmp', 'xlsx', template))
     worksheet = workbook[0]
-    #CSV.parse(rendered_string).each_with_index do |row, i|
-    #  row.each_with_index do |col, j|
-    #    value = case col 
-    #            when /\A\d+\z/
-    #              col.to_i
-    #            when /\A[\d\.]+\z/
-    #              col.to_f
-    #            else
-    #              col 
-    #            end 
-    #    worksheet.add_cell(i, j, value)
-    #  end 
-    #end 
+
+    # https://github.com/weshatheleopard/rubyXL
+    # Possible weights: hairline, thin, medium, thick
+    #
+    # http://www.rubydoc.info/gems/rubyXL/3.3.17/RubyXL
+    # %w{ none thin medium dashed dotted thick double hair
+    # mediumDashed dashDot mediumDashDot dashDotDot slantDashDot }
+    5.upto(10).each do |i|
+      worksheet.add_cell(i, 1, 'hoge')
+      #worksheet.sheet_data[i][1].change_border(:top, 'thin') # 普通の線
+      #worksheet.sheet_data[i][1].change_border(:top, 'hairline') # 線なし
+      #worksheet.sheet_data[i][1].change_border(:top, 'medium') # 太線
+      worksheet.sheet_data[i][1].change_border(:top, 'dotted') # 点線
+    end
+
     FileUtils.mkdir_p(Rails.root.join('tmp', 'csv'))
     path = Rails.root.join('tmp', 'csv', "#{filename}.xlsx")
     workbook.write(path)
     path
   end
-
 end
+
